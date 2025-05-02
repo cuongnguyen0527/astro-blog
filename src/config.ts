4a763b86c4dcb4ae061d639012c7a4b0a374c577
Cuong Nguyen <cuong.nguyen0527@gmail.com>
2025-05-02 15:13:33 +0700

    Change config for site

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://banhxeo.netlify.app/", // replace this with your deployed domain
+  author: "Nukun",
+  profile: "hhttps://banhxeo.netlify.app/",
+  desc: "Đói thì ăn mà buồn ngủ thì ngủ...",
+  title: "Bánh Xèo",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
